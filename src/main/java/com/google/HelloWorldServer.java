331352e5aca3fa554d589ad6c3be7378cd2874ab
Spencer Carlson <spencer.carlson@student.csulb.edu>
2022-10-17 12:53:32 -0700

    Add more changes.

--- src/main/java/com/google/HelloWorldServer.java
+++ src/main/java/com/google/HelloWorldServer.java
@@ -5,70 +5,82 @@ import services.hello.GreeterGrpc;
 import services.hello.HelloReply;
 import services.hello.HelloRequest;
 import io.grpc.Server;
-import io.grpc.ServerBuilder;
 import io.grpc.stub.StreamObserver;
+import io.grpc.alts.AltsServerBuilder;
 
 import java.io.IOException;
+import java.util.concurrent.Executors;
+import java.util.logging.Level;
 import java.util.logging.Logger;
+
 
 /**
  * Server that manages startup/shutdown of a {@code Greeter} server.
  */
 public class HelloWorldServer {
     private static final Logger logger = Logger.getLogger(HelloWorldServer.class.getName());
-
-    /* The port on which the server should run */
-    private final int port = 8080;
     private Server server;
+    private int port = 8080;
 
-    private void start() throws IOException {
-        server = ServerBuilder.forPort(port)
-                .addService(new GreeterImpl())
-                .build()
-                .start();
-        logger.info("Server started, listening on " + port);
-        Runtime.getRuntime().addShutdownHook(new Thread() {
-            @Override
-            public void run() {
-                // Use stderr here since the logger may have been reset by its JVM shutdown hook.
-                System.err.println("*** shutting down gRPC server since JVM is shutting down");
-                HelloWorldServer.this.stop();
-                System.err.println("*** server shut down");
-            }
-        });
-    }
-
-    private void stop() {
-        if (server != null) {
-            server.shutdown();
-        }
-    }
-
-    /**
-     * Await termination on the main thread since the grpc library uses daemon threads.
-     */
-    private void blockUntilShutdown() throws InterruptedException {
-        if (server != null) {
-            server.awaitTermination();
-        }
-    }
-
-    /**
-     * Main launches the server from the command line.
-     */
     public static void main(String[] args) throws IOException, InterruptedException {
-        final HelloWorldServer server = new HelloWorldServer();
-        server.start();
-        server.blockUntilShutdown();
+        new HelloWorldServer().start(args);
     }
 
-    private class GreeterImpl extends GreeterGrpc.GreeterImplBase {
+    private void parseArgs(String[] args) {
+        boolean usage = false;
+        for (String arg : args) {
+            if (!arg.startsWith("--")) {
+                System.err.println("All arguments must start with '--': " + arg);
+                usage = true;
+                break;
+            }
+            String[] parts = arg.substring(2).split("=", 2);
+            String key = parts[0];
+            if ("help".equals(key)) {
+                usage = true;
+                break;
+            }
+            if (parts.length != 2) {
+                System.err.println("All arguments must be of the form --arg=value");
+                usage = true;
+                break;
+            }
+            String value = parts[1];
+            if ("port".equals(key)) {
+                port = Integer.parseInt(value);
+            } else {
+                System.err.println("Unknown argument: " + key);
+                usage = true;
+                break;
+            }
+        }
+        if (usage) {
+            HelloWorldServer s = new HelloWorldServer();
+            System.out.println(
+                    "Usage: [ARGS...]"
+                            + "\n"
+                            + "\n  --port=PORT           Server port to bind to. Default "
+                            + s.port);
+            System.exit(1);
+        }
+    }
 
+    private void start(String[] args) throws IOException, InterruptedException {
+        parseArgs(args);
+        server =
+                AltsServerBuilder.forPort(port)
+                        .addService(new GreeterImpl())
+                        .executor(Executors.newFixedThreadPool(1))
+                        .build();
+        server.start();
+        logger.log(Level.INFO, "Started on {0}", port);
+        server.awaitTermination();
+    }
+    static class GreeterImpl extends GreeterGrpc.GreeterImplBase {
         @Override
-        public void sayHello(HelloRequest req, StreamObserver<HelloReply> responseObserver) {
-            HelloReply reply = HelloReply.newBuilder().setMessage("Hello " + req.getName()).build();
-            responseObserver.onNext(reply);
-            responseObserver.onCompleted();
+        public void sayHello(HelloRequest request, StreamObserver<HelloReply> observer) {
+            observer.onNext(HelloReply.newBuilder().setMessage("Hello, " + request.getName()).build());
+            observer.onCompleted();
         }
     }
 }
